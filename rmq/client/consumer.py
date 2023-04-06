bd252b62ebffcb06724f49b665f9184ce0da7a8a
vspaz <vladimir.edelman@gmail.com>
2023-04-06 21:45:30 +0200

    style fixes

--- rmq/client/consumer.py
+++ rmq/client/consumer.py
@@ -2,32 +2,32 @@ import json
 import logging
 
 import aiomisc
-from aio_pika import IncomingMessage
 import uvloop
-from client import RmqClient
+from aio_pika import IncomingMessage
+from client import Client
 
 uvloop.install()
 
 
-async def listen_for_messages(rmq_client: RmqClient):
+async def listen_for_messages(rmq_client: Client):
     async def on_message_received(message: IncomingMessage):
         async with message.process(requeue=True):
             try:
                 resp = json.loads(message.body)
-                logging.error(f'message received: {resp!r}')
+                logging.error(f"message received: {resp!r}")
             except Exception as err:
                 logging.error(err)
 
     await rmq_client.subscribe(
-        queue_name='test',
+        queue_name="test",
         on_message=on_message_received,
     )
 
 
 def run():
     with aiomisc.entrypoint() as loop:
-        rmq_client = RmqClient()
-        logging.info('rabbitmq client initialized')
+        rmq_client = Client()
+        logging.info("rabbitmq client initialized")
         loop.create_task(listen_for_messages(rmq_client=rmq_client))
         loop.run_forever()
 
@@ -36,5 +36,5 @@ def main():
     run()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
